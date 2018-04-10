9c3c62e08f0576256a31dc18cacf68e1ee880fbd
Leandro Perini <perini105@gmail.com>
2018-04-09 22:06:47 -0300

    ajustes de configuração de assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/*', 'public/js');
+mix.js('resources/assets/js/functions.js', 'public/js');
+
+mix.less('resources/assets/less/app.less', 'public/css').options({
+    "javascriptEnabled": true
+});
