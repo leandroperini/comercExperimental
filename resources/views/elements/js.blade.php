24f17efd545f48bfcc057f465aa92e46c5e3fb8b
Leandro Perini <perini105@gmail.com>
2018-04-11 00:57:46 -0300

    removendo arquivos desnecessários

--- resources/views/elements/js.blade.php
+++ resources/views/elements/js.blade.php
@@ -1,11 +1,8 @@
 <!-- Scripts -->
 <script src="{{ asset('vendors/bower_components/jquery/dist/jquery.min.js') }}" defer></script>
 <script src="{{ asset('vendors/bower_components/bootstrap/dist/js/bootstrap.min.js') }}" defer></script>
-<script src="{{ asset('vendors/bower_components/flot/jquery.flot.js') }}" defer></script>
-<script src="{{ asset('vendors/bower_components/flot.curvedlines/curvedLines.js') }}" defer></script>
 <script src="{{ asset('vendors/sparklines/jquery.sparkline.min.js') }}" defer></script>
 <script src="{{ asset('vendors/bower_components/moment/min/moment.min.js') }}" defer></script>
-<script src="{{ asset('vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js') }}" defer></script>
 <script src="{{ asset('vendors/bower_components/Waves/dist/waves.min.js') }}" defer></script>
 <script src="{{ asset('vendors/bootstrap-growl/bootstrap-growl.min.js') }}" defer></script>
 <script src="{{ asset('vendors/bower_components/bootstrap-sweetalert/lib/sweet-alert.min.js') }}" defer></script>
